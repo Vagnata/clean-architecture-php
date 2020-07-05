10034cbd87494afa1ff052933d486e17cfb27aae
Vagner Garcia <garciavagnata.vg@gmail.com>
2020-07-05 13:57:49 -0400

    Implementa classe de teste para Student

---
+++ tests/StudentTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Alura\Arquitetura\Testes;
+
+use Alura\Arquitetura\Student;
+
+class StudentTest extends BaseTestCase
+{
+    public function testPhoneNumberAddition()
+    {
+        $areaCode    = $this->faker->areaCode();
+        $phoneNumber = $this->faker->cellphone(false);
+
+        $student = new Student();
+        $student->addPhoneNumber($areaCode, $phoneNumber);
+
+        $this->assertEquals($areaCode . $phoneNumber, current($student->getPhones())->__toString());
+    }
+}
