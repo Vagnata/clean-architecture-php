52e383333f544ba2c5f0880d5674b176a0595ba1
Vagner Garcia <garciavagnata.vg@gmail.com>
2020-07-05 13:55:36 -0400

    Adicionar providers do faker para dados pt-BR

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -5,6 +5,9 @@ namespace Alura\Arquitetura\Testes;
 
 use Faker\Factory;
 use Faker\Generator;
+use Faker\Provider\pt_BR\Address;
+use Faker\Provider\pt_BR\Person;
+use Faker\Provider\pt_BR\PhoneNumber;
 use PHPUnit\Framework\TestCase;
 
 class BaseTestCase extends TestCase
@@ -18,5 +21,8 @@ class BaseTestCase extends TestCase
     {
         parent::setUp();
         $this->faker = Factory::create();
+        $this->faker->addProvider(new PhoneNumber($this->faker));
+        $this->faker->addProvider(new Person($this->faker));
+        $this->faker->addProvider(new Address($this->faker));
     }
 }
